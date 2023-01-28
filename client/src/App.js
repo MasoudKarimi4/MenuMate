1cda32c79b3589b4817a45d3baa3c0bea8af627b
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-01-28 14:40:51 -0500

    wrork

--- client/src/App.js
+++ client/src/App.js
@@ -16,20 +16,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MenuMate <br></br> McHacks 2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -15,3 +15,4 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+
