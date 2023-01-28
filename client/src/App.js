7306ec310d70bb1ae15e8ac5f08dfa8642f9411c
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-01-28 13:37:38 -0500

    took off the text

--- client/src/App.js
+++ client/src/App.js
@@ -13,22 +13,15 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Menu Mate <br></br>Submission for McHacks 2023
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
