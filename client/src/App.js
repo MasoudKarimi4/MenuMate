fa573a37943a41209a29ddd0313aae4ad0f9e30f
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-01-28 14:28:44 -0500

    adding

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
