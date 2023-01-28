a9cec51012a6defa7f7ddcd393993992dbb7e642
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-01-28 13:30:18 -0500

    Node & React

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Server and Api stuff
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
