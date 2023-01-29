be27ede6cf441f3bf5d5a310d0b5707bc7dffcc2
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-01-29 01:24:53 -0500

    IT WORKED

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import {frontEnd} from './frontend.js'
+import cors from 'cors';
+
+const Test = () => {
+  const [value, setValue] = useState(undefined);
+
+  
+  
+  const getValue = async () => {
+    console.log("it worked")
+    try {
+      const response = await fetch('http://localhost:5000/my-endpoint');
+      const data = await response.json();
+      setValue(data);
+      console.log("Trying to fetch data")
+      console.log(data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  return (
+    <div>
+      <button onClick={getValue}>Get Value</button>
+      <p>yo</p>
+
+      {value && <p>Value: {value}</p>}
+    </div>
+  );
+}
 
 class MyComponent extends React.Component {
+
+
   constructor(props) {
     super(props);
     this.state = { name: 'John Doe' };
@@ -22,23 +54,33 @@ class MyComponent extends React.Component {
 function App() {
 
 
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          MenuMate <br></br> McHacks 2023
-        </p>
+      <body>
+      <div className='Title'>
+        MenuMate.
+      </div>
+      <div className='Subtitle'>
+        Finding a good meal <br></br> Just got better.
+      </div>
+      <div className='MainText'>
+        Welcome to MenuMate, your number one provider focused on evaluating the best restaurants for you. With the click of a button, MenuMate scoures and assembles data from various sources to provide you with the most up-to-date and accurate information on the restaurants in your area. Browse through our database and find your perfect meal today!
+      </div>
+      <div className='SearchText'>
+        <br></br> Use the search bar below to find your next meal today!
+      </div>
+      <div className='SearchBar'>
+        <form class="search" action="">
+          <input type="search" placeholder="Search here..." required/>
+            <button type="submit">Search</button>
+        </form>
+
+      </div>
+    < Test />
+  </body>
       </header>
 
-      <MyComponent />
     </div>
 
 
@@ -46,3 +88,5 @@ function App() {
 }
 
 export default App;
+
+
--- server/nlp.py
+++ server/nlp.py
@@ -16,11 +16,26 @@ response = co.generate(
 summary = response.generations[0].text
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/get-variable", methods=['GET'])
-def get_variable():
+
+
+@app.route('/my-endpoint', methods=['GET'])
+def hello():
+    restaurant = '1 for 1 pizza'
+
+    prompt = 'Write about '+restaurant+", an Ottawa restaurant."
+
+    response = co.generate( 
+        model='command-xlarge-nightly', 
+        prompt = prompt,
+        max_tokens=500, 
+        temperature=0.8,
+        stop_sequences=["--"])
+
+    summary = response.generations[0].text
     return jsonify(summary)
 
-
+CORS(app)
